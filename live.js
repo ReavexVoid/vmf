dffa0c3e32f897bc2d288600bf6b4b3fe244afe7
ReavexVoid <reavexfromvoid6@gmail.com>
2026-01-31 18:21:54 +0100

    Update live.js

--- live.js
+++ live.js
@@ -5,3 +5,9 @@ setInterval(() => {
   ramUse.textContent = vm.usage.ram.toFixed(1) + "%";
   diskUse.textContent = vm.usage.storage.toFixed(1) + "%";
 }, 500);
+
+export async function importVMF(file) {
+  const text = await file.text();
+  const encrypted = JSON.parse(text);
+  return await decryptObject(encrypted);
+}
